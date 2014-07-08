bd8f17e848c250229d3aec7de5ac977e50079ce4
arrdem <me@arrdem.com>
2014-07-07 21:13:37 -0500

    Rework into lein project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject grimoire "0.1.0-SNAPSHOT"
+  :description "THIS SHOULD NEVER BE DEPLOYED"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.thnetos/cd-client "0.3.6"]]
+
+  :main grimoire.core
+
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})

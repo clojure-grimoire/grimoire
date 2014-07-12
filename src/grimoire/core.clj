de5effbb1eaeb877c8b99fe6d5860cde506914e5
arrdem <me@arrdem.com>
2014-07-12 10:52:48 -0500

    Fix some sins

--- src/grimoire/core.clj
+++ src/grimoire/core.clj
@@ -104,17 +104,18 @@
 
       ;; write docstring file
       (let [inc-doc-file (file sym-inc-dir "docs.md")]
-        (->> (format (str "## Arities\n"
-                          "%s\n\n"
-                          "## Documentation\n"
-                          "{%%raw%%}\n"
-                          "%s\n"
-                          "{%%endraw%%}\n")
-                     (->> arglists
-                          (interpose \newline)
-                          (reduce str))
-                     doc)
-             (spit inc-doc-file)))
+        (when-not (.exists inc-doc-file)
+          (->> (format (str "## Arities\n"
+                            "%s\n\n"
+                            "## Documentation\n"
+                            "{%%raw%%}\n"
+                            "%s\n"
+                            "{%%endraw%%}\n")
+                       (->> arglists
+                            (interpose \newline)
+                            (reduce str))
+                       doc)
+               (spit inc-doc-file))))
 
       (when src
         ;; write source file
@@ -265,7 +266,7 @@
 (defn write-docs-for-ns
   [dirs ns]
   (let [[version-dir include-dir]         dirs
-        ns-vars                           (->> (ns-publics ns) keys (remove var-blacklist))
+        ns-vars                           (->> (ns-publics ns) vals (remove var-blacklist))
         macros                            (filter macro? ns-vars)
         fns                               (filter #(and (fn? @%1)
                                                         (not (macro? %1)))
@@ -281,11 +282,7 @@
 
       ;; write per symbol docs
       (doseq [var ns-vars]
-        (try
-          (write-docs-for-var files var)
-          (println "Documented" var)
-          (catch java.lang.AssertionError e
-            (println "Warning: Failed to write docs for" var))))
+        (write-docs-for-var files var))
 
       (when (= ns 'clojure.core)
         (write-docs-for-specials files))

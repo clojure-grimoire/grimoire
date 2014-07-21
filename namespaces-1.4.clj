9f23b15623560169e57cbd8126f0a8dc8469b7df
arrdem <me@arrdem.com>
2014-07-20 23:09:57 -0500

    Add namespace list files
    Fix #8

---
+++ namespaces-1.4.clj
@@ -0,0 +1,27 @@
+[;; Clojure "core"
+ clojure.core
+ clojure.data
+ clojure.instant
+ clojure.pprint
+ clojure.reflect
+ clojure.repl
+ clojure.set
+ clojure.stacktrace
+ clojure.string
+ clojure.template
+ clojure.test
+ clojure.uuid
+ clojure.walk
+ clojure.xml
+ clojure.zip
+ 
+ ;; Clojure JVM host interop
+ clojure.java.browse
+ clojure.java.io
+ clojure.java.javadoc
+ clojure.java.shell
+ 
+ ;; Clojure test
+ clojure.test.junit
+ clojure.test.tap
+ ]
---
+++ namespaces.clj
@@ -0,0 +1,28 @@
+[;; Clojure "core"
+ clojure.core
+ clojure.data
+ clojure.edn
+ clojure.instant
+ clojure.pprint
+ clojure.reflect
+ clojure.repl
+ clojure.set
+ clojure.stacktrace
+ clojure.string
+ clojure.template
+ clojure.test
+ clojure.uuid
+ clojure.walk
+ clojure.xml
+ clojure.zip
+ 
+ ;; Clojure JVM host interop
+ clojure.java.browse
+ clojure.java.io
+ clojure.java.javadoc
+ clojure.java.shell
+ 
+ ;; Clojure test
+ clojure.test.junit
+ clojure.test.tap
+ ]

d4e1bd0b560844f914a4f58ebff64322abe62fe1
arrdem <me@arrdem.com>
2014-08-02 01:50:09 -0500

    Fix the snapshot script

--- snapshot.sh
+++ snapshot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-lein serve &
+lein serve 4000 &
 PID=$!
 sleep 10
 
-wget -r -p -P resources/static/ http://127.0.0.1:3000/
+wget -r -p -nH -P resources/static/ http://127.0.0.1:4000/
 zip -r resources/public/static.zip resources/static
 rm -r resources/static
 
 kill $PID
-

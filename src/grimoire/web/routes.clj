7f99057678832c6a9c9f1a6da0a0dbcc456d1a0e
arrdem <me@arrdem.com>
2014-08-06 16:54:14 -0500

    Clean up routes to be more explicit about paths

--- src/grimoire/web/routes.clj
+++ src/grimoire/web/routes.clj
@@ -20,38 +20,37 @@
 
   (route/resources "/public")
 
-  (context "/:version" [version]
-    (GET "/" [version]
-         (when (#{"1.4.0" "1.5.0" "1.6.0"} version)
-           (views/version-page version)))
+  (GET "/:version/" [version]
+       (if (#{"1.4.0" "1.5.0" "1.6.0"} version)
+         (views/version-page version)
+         (views/error-unknown-version version)))
 
-    (context "/:namespace" [namespace]
-      (GET "/" [version namespace]
-           (views/namespace-page-memo version namespace))
+  (GET "/:version/:namespace/" [version namespace]
+       (views/namespace-page-memo version namespace))
 
-      (context "/:symbol" [symbol]
-        (GET "/" {header-type :content-type
-                  {param-type :type} :params
-                  :as req}
-             (if (#{"catch" "finally"} symbol)
-               (response/redirect (str "/" version "/clojure.core/try"))
-               (views/symbol-page version namespace symbol
-                                  (keyword (or header-type param-type "text/html")))))
+  (GET "/:version/:namespace/:symbol/"
+       {header-type :content-type
+        {param-type :type} :params
+        [version namespace symbol] :params
+        :as req}
+       (if (#{"catch" "finally"} symbol)
+         (response/redirect (str "/" version "/clojure.core/try/"))
+         (views/symbol-page version namespace symbol
+                            (keyword (or header-type param-type "text/html")))))
 
-      (GET "/docstring" []
-           (util/resource-file-contents
-            (str "resources/" version "/" namespace "/" symbol "/docstring.md")))
+  (GET "/:version/:namespace/:symbol/docstring" [version namespace symbol]
+       (util/resource-file-contents
+        (str "resources/" version "/" namespace "/" symbol "/docstring.md")))
 
-      (GET "/extended-docstring" []
-           (util/resource-file-contents
-            (str "resources/" version "/" namespace "/" symbol "/extended-docstring.md")))
+  (GET "/:version/:namespace/:symbol/extended-docstring" [version namespace symbol]
+       (util/resource-file-contents
+        (str "resources/" version "/" namespace "/" symbol "/extended-docstring.md")))
 
-      (GET "/related" []
-           (util/resource-file-contents
-            (str "resources/" version "/" namespace "/" symbol "/related.txt")))
+  (GET "/:version/:namespace/:symbol/related" [version namespace symbol]
+       (util/resource-file-contents
+        (str "resources/" version "/" namespace "/" symbol "/related.txt")))
 
-      (GET "/examples" []
-           (views/all-examples version namespace symbol :text)))))
+  (GET "/:version/:namespace/:symbol/examples" [version namespace symbol]
+       (views/all-examples version namespace symbol :text))
 
   (route/not-found (views/error-404)))
-

2633ed2c44ce47a24d9fc9000c929fd70d5af6bc
arrdem <me@arrdem.com>
2014-08-08 18:39:55 -0500

    Need to add that dep..

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                       :main         grimoire.doc}
              :repl   {:plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
              :server {:dependencies [[org.clojure/clojure "1.6.0"]
+                                     [com.taoensso/timbre "3.2.1"]
                                      [ring/ring-core "1.3.0"]
                                      [ring/ring-jetty-adapter "1.3.0"]
                                      [selmer "0.6.9"]

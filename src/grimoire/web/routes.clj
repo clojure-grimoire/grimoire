e81718b70fc3f3b9fe71a5e47f131ecfc99fe16a
arrdem <me@arrdem.com>
2014-10-09 12:32:46 -0500

    Fix #117

--- src/grimoire/web/routes.clj
+++ src/grimoire/web/routes.clj
@@ -41,49 +41,69 @@
                          (views/namespace-page-memo version namespace))
 
                     (context "/:symbol" [symbol]
-                             (GET "/" {header-type :content-type
-                                       {param-type :type} :params
-                                       :as req
-                                       uri :uri}
-                                  (let [type (or header-type param-type :html)]
-                                    (if (#{"catch" "finally"} symbol)
-                                      (response/redirect (str "/" version "/clojure.core/try/"))
-                                      (if-let [res (views/symbol-page version namespace symbol type)]
-                                        (do (info (pr-str {:uri uri :type type})) res)
-                                        (let [new-uri (str "/" version "/" namespace "/" (util/unmunge symbol))]
-                                          (when-not (= new-uri uri)
-                                            (response/redirect new-uri)))))))
+                      (GET "/" {header-type :content-type
+                                {param-type :type} :params
+                                :as req
+                                uri :uri}
+                        (let [type (or header-type param-type :html)]
+                          (if (#{"catch" "finally"} symbol)
+                            (response/redirect (str "/" version "/clojure.core/try/"))
+                            (if-let [res (views/symbol-page version namespace symbol type)]
+                              (do (info (pr-str {:uri uri :type type})) res)
+                              (let [new-uri (str "/" version "/" namespace "/" (util/unmunge symbol))]
+                                (when-not (= new-uri uri)
+                                  (response/redirect new-uri)))))))
 
-                             (GET "/docstring" {uri :uri}
-                                  (let [f  (util/resource-file version namespace symbol "docstring.md")]
-                                    (when (and f (.isFile f))
-                                      (info (pr-str {:uri uri :type :text}))
-                                      (slurp f))))
+                      (GET "/docstring" {uri :uri}
+                        (let [f  (util/resource-file version namespace symbol "docstring.md")]
+                          (when (and f (.isFile f))
+                            (info (pr-str {:uri uri :type :text}))
+                            (slurp f))))
 
-                             (GET "/extended-docstring" {uri :uri}
-                                  (let [f (util/resource-file version namespace symbol "extended-docstring.md")]
-                                    (when (and f (.isFile f))
-                                      (info (pr-str {:uri uri :type :text}))
-                                      (slurp f))))
+                      (GET "/extended-docstring" {uri :uri}
+                        (let [f (util/resource-file version namespace symbol "extended-docstring.md")]
+                          (when (and f (.isFile f))
+                            (info (pr-str {:uri uri :type :text}))
+                            (slurp f))))
 
-                             (GET "/related" {uri :uri}
-                                  (let [f (util/resource-file version namespace symbol "related.txt")]
-                                    (when (and f (.isFile f))
-                                      (info (pr-str {:uri uri :type :text}))
-                                      (slurp f))))
+                      (GET "/related" {uri :uri}
+                        (let [f (util/resource-file version namespace symbol "related.txt")]
+                          (when (and f (.isFile f))
+                            (info (pr-str {:uri uri :type :text}))
+                            (slurp f))))
 
-                             (GET "/examples" {uri :uri}
-                                  (when-let [examples (views/all-examples version namespace symbol :text)]
-                                    (info (pr-str {:uri uri :type :text}))
-                                    examples))
+                      (GET "/examples" {uri :uri}
+                        (when-let [examples (views/all-examples version namespace symbol :text)]
+                          (info (pr-str {:uri uri :type :text}))
+                          examples))
 
-                             (route/not-found
-                              (fn [{uri :uri
-                                   header-type :content-type
-                                   {param-type :type} :params}]
-                                (let [type (or header-type param-type :text/html)]
-                                  (warn (pr-str {:uri uri}))
-                                  (views/error-unknown-symbol type version namespace symbol)))))
+                      (GET "/" {header-type :content-type
+                                {param-type :type} :params
+                                :as req
+                                uri :uri}
+                        (let [type    (or header-type param-type :html)
+                              symbol' (util/unmunge symbol)]
+                          (cond
+                           ;; FIXME this is a bit of a hack to handle catch/finally
+                           (#{"catch" "finally"} symbol)
+                           ,,(response/redirect (str "/" version "/clojure.core/try/"))
+
+                           ;; handle the case of redirecting due to munging
+                           (not (= symbol symbol'))
+                           ,,(response/redirect (str "/" version "/" namespace "/" symbol' "/"))
+
+                           :else
+                           ,,(let [res (views/symbol-page version namespace symbol type)]
+                               (info (pr-str {:uri uri :type type}))
+                               res))))
+
+                      (route/not-found
+                       (fn [{uri :uri
+                            header-type :content-type
+                            {param-type :type} :params}]
+                         (let [type (or header-type param-type :text/html)]
+                           (warn (pr-str {:uri uri}))
+                           (views/error-unknown-symbol type version namespace symbol)))))
 
                     (route/not-found
                      (fn [{uri :uri}]

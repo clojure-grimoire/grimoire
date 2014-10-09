1eb269f925ccca98df82f097077e82969d5f4d03
arrdem <me@arrdem.com>
2014-10-09 12:33:06 -0500

    Start thinking about sessions

--- src/grimoire/web/service.clj
+++ src/grimoire/web/service.clj
@@ -1,13 +1,19 @@
 (ns grimoire.web.service
   (:require [compojure.handler :as handler]
+            [ring.middleware.session :as session]
             [grimoire.web.routes :refer [app]]
             [ring.adapter.jetty :as jetty]))
 
 (defonce service (atom nil))
 
+(def handler
+  (-> app
+      handler/site
+      session/wrap-session))
+
 (defn start-web-server! [& [port?]]
   (reset! service
-          (jetty/run-jetty (handler/site app)
+          (jetty/run-jetty handler
                            {:port (or port? 3000)
                             :join? false})))
 
@@ -17,6 +23,9 @@
     (reset! service nil)))
 
 (defn restart-web-server! [& [port?]]
+  (require 'grimoire.web.routes
+           'grimoire.web.service
+           :reload)
   (stop-web-server!)
   (start-web-server! port?))
 

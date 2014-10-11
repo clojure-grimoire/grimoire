5a6dd03e01d9baf54badb4cb067324b48f2e28a2
arrdem <me@arrdem.com>
2014-10-11 14:14:49 -0500

    WIP

---
+++ src/grimoire/web/api.clj
@@ -0,0 +1,6 @@
+(ns grimoire.web.api
+  "This namespace serves to implement an abstraction layer for looking
+  up examples, symbols, namespaces and artifacts as values without
+  regard to the implementation of the datastore. Ports of Grimoire to
+  different datastores should only need to tweak this namespace.")
+

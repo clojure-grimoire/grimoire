11f96d158665686fb8780fce6fcdab7a04fd2b98
arrdem <me@arrdem.com>
2014-11-23 16:06:41 -0600

    First cut at multimethod dispatch on request type

--- src/grimoire/web/views.clj
+++ src/grimoire/web/views.clj
@@ -11,9 +11,6 @@
             [ring.util.response :as response]
             [detritus.text :as text]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Site config
-
 (def site-config
   {:url                 "http://conj.io"
    :repo                "https://github.com/clojure-grimoire/grimoire"
@@ -32,10 +29,9 @@
                          :description "Community documentation of Clojure"
                          :quote       "Even the most powerful wizard must consult grimoires as an aid against forgetfulness."}})
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Generic markdown page
-
-(defn markdown-page [page]
+(defn markdown-page
+  "Helper for rendering a markdown page off of the resource path as HTML"
+  [page]
   (let [[header page] (wutil/parse-markdown-page page)]
     (layout
      site-config
@@ -45,25 +41,16 @@
              page)
        (response/not-found "Resource not found, sorry. Please file an issue on the github bugtracker.")))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Home page
-
 (defn home-page []
   (layout
    site-config
    [:blockquote [:p (-> site-config :style :quote)]]
    (wutil/cheatsheet-memo site-config)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Shared header generator
-
 (defn link-to [prefix x]
   {:href (str prefix (:uri x))})
 
 (def link-to' (partial link-to "/store/"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Articles list page
 
 (defn articles-list []
   (layout site-config
@@ -73,9 +60,6 @@
                  :let  [[_articles a] p
                         a             (string/replace a ".md" "")]]
              [:li [:a (link-to "articles" a) a]])]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Header helper
 
 (defmulti header :type)
 
@@ -104,11 +88,14 @@
           [:a (link-to' symbol)
            ,,,(:name symbol)])))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; API page
+(defmulti store-page identity)
 
-;; FIXME: refactor to display preview from notes
-(defn api-page []
+;; FIXME: text/zalgo
+;; FIXME: text/plain
+;; FIXME: application/json
+;; FIXME: application/edn
+
+(defmethod store-page :text/html [_]
   (layout site-config
           [:h1 {:class "page-title"}
            ,,"Artifact store"]
@@ -118,10 +105,20 @@
                             (sort-by :name))]
              [:li [:a (link-to' group) (:name group)]])]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Groupid page
 
-(defn groupid-page [group-thing]
+(def dispatch-fn
+  (fn [x y]
+    {:pre [(and x y)
+           (keyword? x)]}
+    x))
+
+(defmulti group-page dispatch-fn)
+
+;; FIXME: text/zalgo
+;; FIXME: application/edn
+;; FIXME: application/json
+
+(defmethod group-page :text/html [_ group-thing]
   (layout site-config
           [:h1 {:class "page-title"} (header group-thing)]
           [:h2 "Known artifacts"]
@@ -132,10 +129,16 @@
              [:li [:a (link-to' artifact)
                    (format "%s/%s" (:name group) (:name artifact))]])]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Artifactid page
 
-(defn artifactid-page [artifact-thing]
+
+(defmulti artifact-page dispatch-fn)
+
+;; FIXME: text/zalgo
+;; FIXME: text/plain
+;; FIXME: application/edn
+;; FIXME: application/json
+
+(defmethod artifact-page :text/html [_ artifact-thing]
   (layout site-config
           [:h1 {:class "page-title"} (header artifact-thing)]
           [:h2 "Known release versions"]
@@ -150,10 +153,15 @@
                                                  (:name artifact)
                                                  (:name version))]])]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Version page
 
-(defn version-page [version-thing]
+(defmulti version-page dispatch-fn)
+
+;; FIXME: text/zalgo
+;; FIXME: text/plain
+;; FIXME: application/edn
+;; FIXME: application/json
+
+(defmethod version-page :text/html [_ version-thing]
   (let [[[v notes]] (api/read-notes site-config version-thing)]
     (layout
      site-config ;; FIXME: add artifact & group name to title somehow?
@@ -169,8 +177,6 @@
         [:li
          [:a (link-to' ns-thing) (:name ns-thing)]])])))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Namespace page
 
 (defn emit-alphabetized-links [records]
   (let [segments (group-by (comp first str :name) records)]
@@ -179,14 +185,33 @@
             [:p (for [r (sort-by :name (get segments k))]
                   [:a {:href (:url r) :style "padding: 0 0.2em;"} (:name r)])]))))
 
-(defn namespace-page [namespace-thing]
+(defmulti namespace-page dispatch-fn)
+
+;; FIXME: text/zalgo
+;; FIXME: text/plain
+;; FIXME: application/edn
+;; FIXME: application/json
+
+(defmethod namespace-page :text/html [_ namespace-thing]
   (layout
    site-config ;; FIXME: add artifact, namespace?
    [:h1 {:class "page-title"}
     (header namespace-thing)]
-   (let [[[v notes]] (api/read-notes site-config namespace-thing)]
+
+   (let [{:keys [doc name] :as meta} (api/read-meta site-config namespace-thing)
+         doc                         (-> doc
+                                         text/text->paragraphs
+                                         text/render)]
+     (when doc
+       (list [:h2 "Namespace Docs"]
+             [:p doc])))
+
+   (when-let [notes (first
+                     (for [[v notes] (api/read-notes site-config namespace-thing)
+                           :when     (= v (:name (:parent namespace-thing)))]
+                       notes))]
      (list [:h2 "Namespace Notes"]
-           (wutil/markdown-string notes)))
+           [:p (wutil/markdown-string notes)]))
 
    (list [:h2 "Symbols"]
          ;; FIXME: the fuck am I doing here srsly
@@ -222,12 +247,14 @@
 (def namespace-page-memo
   (memoize namespace-page))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Symbol page
 
-;; FIXME: finish refactoring to use lib-grimoire
-(defn symbol-page
-  [resp-type def-thing]
+(defmulti symbol-page dispatch-fn)
+
+;; FIXME: text/zalgo
+;; FIXME: application/edn
+;; FIXME: application/json
+
+(defmethod symbol-page :text/html [_ def-thing]
   (let [groupid      (t/thing->group     def-thing)
         artifactid   (t/thing->artifact  def-thing)
         version      (t/thing->version   def-thing)
@@ -236,109 +263,129 @@
         {:keys [doc name type arglists src]
          :as   meta} (api/read-meta site-config def-thing)
 
+        doc          (-> doc
+                         text/text->paragraphs
+                         text/render)
+
+        notes        (-> site-config (api/read-notes def-thing)) ;; Seq [version, notes]
+
+        related      (-> site-config
+                         (api/read-related def-thing))]  ;; Seq [version, related]
+    (when doc
+      (layout
+       (assoc site-config
+         :page {:description (str "Clojure " version " " namespace "/" symbol
+                                  " documentation and examples")
+                :summary doc})
+       [:h1 {:class "page-title"}
+        (header def-thing)]
+
+       (when arglists
+         (list [:h2 "Arities"]
+               [:pre (->> arglists
+                          (map pr-str)
+                          (map #(str "   " %))
+                          (interpose \newline))]))
+
+       (when doc
+         (list [:h2 "Official Documentation"]
+               [:pre doc]))
+
+       (when notes
+         (list
+          [:h2 "Community Documentation"]
+          ;; FIXME: Add edit URL!
+          (for [[v text] notes]
+            [:p (-> text
+                    text/text->paragraphs
+                    text/render)])))
+
+       ;; FIXME: examples needs a _lot_ of work
+       (when-let [examples (api/read-examples site-config def-thing)]
+         [:div.section
+          [:h2.heading "Examples " [:span.hide "-"]]
+          [:div.autofold
+           (for [[v e] examples]
+             [:div.example
+              [:div.source
+               (wutil/highlight-clojure e)]])
+           ;; FIXME: Add example link!
+           ]])
+
+
+       (when-not (= :special type)
+         [:a {:href (str "http://crossclj.info/fun/" (:name namespace) "/" (wutil/url-encode name) ".html")}
+          [:h2 "Uses on crossclj"]])
+
+       (when related
+         (list [:h2 "Related Symbols"]
+               [:ul (for [r related]
+                      (let [[ns sym] (string/split r #"/")]
+                        [:li [:a {:href (str (:baseurl site-config)
+                                             "/" version "/" ns "/"
+                                             (util/munge sym) "/")}
+                              r]]))]))
+
+       (when src
+         (list
+          [:div.section
+           [:h2.heading "Source " [:span.unhide "+"]]
+           [:div.autofold.prefold
+            (wutil/highlight-clojure src)]]))
+
+       [:script {:src "/public/jquery.js" :type "text/javascript"}]
+       [:script {:src "/public/fold.js" :type "text/javascript"}]))))
+
+
+(defmethod symbol-page :text/plain [_ def-thing]
+  (let [groupid      (t/thing->group     def-thing)
+        artifactid   (t/thing->artifact  def-thing)
+        version      (t/thing->version   def-thing)
+        namespace    (t/thing->namespace def-thing)
+
+        {:keys [doc name type arglists src]
+         :as   meta} (api/read-meta site-config def-thing)
+
+        doc          (-> doc
+                         text/text->paragraphs
+                         text/render)
+
         notes        (-> site-config (api/read-notes def-thing)) ;; Seq [version, notes]
 
         related      (-> site-config
                          (api/read-related def-thing))  ;; Seq [version, related]
-        ]
-    (case resp-type
-      (:html :text/html "text/html")
-      ,,(when doc
-          (layout
-           (assoc site-config
-             :page {:description (str "Clojure " version " " namespace "/" symbol
-                                      " documentation and examples")
-                    :summary doc})
-           [:h1 {:class "page-title"}
-            (header def-thing)]
+        
+        line80 (apply str (repeat 80 "-"))
+        line40 (apply str (repeat 40 "-"))]
+    (when doc
+      ;; FIXME: else what? doesn't make sense w/o doc...
+      ;; FIXME: conditionalize _all_ headers
+      (-> (str (format "# [%s/%s \"%s\"] %s/%s\n" groupid artifactid version namespace name)
+               ;; line80
+               "\n"
 
-           (when arglists
-             (list [:h2 "Arities"]
-                   [:pre (interpose \newline (map pr-str arglists))]))
+               "## Arities\n"
+               ;; line40 "\n"
+               (->> (map #(format "  %s\n" (pr-str %1)))
+                    (apply str))
+               "\n"
 
-           (when doc
-             (list [:h2 "Official Documentation"]
-                   [:pre (-> doc
-                             text/text->paragraphs
-                             text/render)]))
+               "## Documentation\n"
+               ;; line40 "\n"
+               doc
+               "\n"
 
-           (when notes
-             (list
-              [:h2 "Community Documentation"]
-              ;; FIXME: Add edit URL!
-              (for [[v text] notes]
-                [:p (-> text
-                        text/text->paragraphs
-                        text/render)])))
+               "## User Documentation\n"
+               ;; line40 "\n"
+               notes
+               "\n"
 
-           ;; FIXME: examples needs a _lot_ of work
-           (when-let [examples (api/read-examples site-config def-thing)]
-             [:div.section
-              [:h2.heading "Examples " [:span.hide "-"]]
-              [:div.autofold
-               (for [[v e] examples]
-                 [:div.example
-                  [:div.source
-                   (wutil/highlight-clojure e)]])
-               ;; FIXME: Add example link!
-               ]])
+               "## Examples\n"
+               ;; line40 "\n"
+               "FIXME: This version of Grimoire can't read examples for text format!\n"
 
-
-           (when-not (= :special type)
-             [:a {:href (str "http://crossclj.info/fun/" (:name namespace) "/" (wutil/url-encode name) ".html")}
-              [:h2 "Uses on crossclj"]])
-
-           (when related
-             (list [:h2 "Related Symbols"]
-                   [:ul (for [r related]
-                          (let [[ns sym] (string/split r #"/")]
-                            [:li [:a {:href (str (:baseurl site-config)
-                                                 "/" version "/" ns "/"
-                                                 (util/munge sym) "/")}
-                                  r]]))]))
-
-           (when src
-             (list
-              [:div.section
-               [:h2.heading "Source " [:span.unhide "+"]]
-               [:div.autofold.prefold
-                (wutil/highlight-clojure src)]]))
-
-           [:script {:src "/public/jquery.js" :type "text/javascript"}]
-           [:script {:src "/public/fold.js" :type "text/javascript"}]))
-
-      (:text :text/plain "text/plain" "text")
-      ,,(let [line80 (apply str (repeat 80 "-"))
-              line40 (apply str (repeat 40 "-"))]
-          (when doc
-            ;; FIXME: else what? doesn't make sense w/o doc...
-            ;; FIXME: conditionalize _all_ headers
-            (-> (str (format "# [%s/%s \"%s\"] %s/%s\n" groupid artifactid version namespace name)
-                     ;; line80
-                     "\n"
-
-                     "## Arities\n"
-                     ;; line40 "\n"
-                     (->> (map #(format "  %s\n" (pr-str %1)))
-                          (apply str))
-                     "\n"
-
-                     "## Documentation\n"
-                     ;; line40 "\n"
-                     doc
-                     "\n"
-
-                     "## User Documentation\n"
-                     ;; line40 "\n"
-                     notes
-                     "\n"
-
-                     "## Examples\n"
-                     ;; line40 "\n"
-                     "FIXME: This version of Grimoire can't read examples for text format!\n"
-
-                     "## See Also\n"
-                     ;; line40 "\n"
-                     related)
-                response/response
-                (response/content-type "text/plain")))))))
+               "## See Also\n"
+               ;; line40 "\n"
+               related)
+          response/response
+          (response/content-type "text/plain")))))

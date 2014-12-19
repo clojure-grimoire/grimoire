2bedd42c5a524b87d47674f7477c3a8012281bc7
arrdem <me@arrdem.com>
2014-12-19 16:40:23 -0600

    And latest lib-grimoire...

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :aliases {"serve" ["with-profile" "server" "run"]}
   :profiles {:repl   {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}
              :server {:dependencies [[org.clojure/clojure "1.6.0"]
-                                     [org.clojure-grimoire/lib-grimoire "0.5.1"]
+                                     [org.clojure-grimoire/lib-grimoire "0.6.0"]
                                      [cheshire "5.3.1"]
                                      [com.taoensso/timbre "3.2.1"]
                                      [ring/ring-core "1.3.0"]

ff2abd2fe69ba7aed1824daf5b991b674cfbc82b
arrdem <me@arrdem.com>
2014-12-19 02:38:43 -0600

    Implement the API
    Fixes #84

--- src/grimoire/web/routes.clj
+++ src/grimoire/web/routes.clj
@@ -5,6 +5,7 @@
             [grimoire.things :as thing]
             [grimoire.web.views :as v ]
             [grimoire.web.views.errors :as v.e]
+            [grimoire.web.views.api :as v.api]
             [ring.util.response :as response]
             [taoensso.timbre :as timbre :refer [info warn]]))
 
@@ -30,130 +31,135 @@
        :as req
        uri :uri}]
     (->> (let-routes [type    (normalize-type (or header-type param-type :html))
-                      log-msg (pr-str {:uri uri :type type})]
-             (context ["/store"] []
-               (GET "/" {uri :uri}
-                 (info (pr-str {:uri uri :type :text}))
-                 (v/store-page type))
-        
-               (context ["/:groupid"] [groupid]
-                 (let-routes [t (thing/->Group groupid)]
-                     (GET "/" []
-                       (info log-msg)
-                       (v/group-page type t))
+                    log-msg (pr-str {:uri uri :type type})]
+         (context ["/store"] []
+           (GET "/" {uri :uri}
+             (info (pr-str {:uri uri :type :text}))
+             (v/store-page type))
 
-                   (context ["/:artifactid"] [artifactid]
-                     (let-routes [t (thing/->T :artifact t artifactid)]
-                         (GET "/" []
-                           (info log-msg)
-                           (v/artifact-page type t))
+           (context ["/:groupid"] [groupid]
+             (let-routes [t (thing/->Group groupid)]
+               (GET "/" []
+                 (info log-msg)
+                 (v/group-page type t))
 
-                       (context ["/:version", :version #"[0-9]+.[0-9]+.[0-9]+"] [version]
-                         (let-routes [t (thing/->T :version t version)]
-                             (GET "/" []
-                               (info log-msg)
-                               (v/version-page type t))
+               (context ["/:artifactid"] [artifactid]
+                 (let-routes [t (thing/->T :artifact t artifactid)]
+                   (GET "/" []
+                     (info log-msg)
+                     (v/artifact-page type t))
 
-                           (context "/:namespace" [namespace]
-                             (let-routes [t (thing/->T :namespace t namespace)]
-                                 (GET "/" []
-                                   (info log-msg)
-                                   (v/namespace-page-memo type t))
+                   (context ["/:version", :version #"[0-9]+.[0-9]+.[0-9]+"] [version]
+                     (let-routes [t (thing/->T :version t version)]
+                       (GET "/" []
+                         (info log-msg)
+                         (v/version-page type t))
 
-                               (context "/:symbol" [symbol]
-                                 (let-routes [t (thing/->T :def t symbol)]
-                                     (GET "/" []
-                                       (let [symbol' (util/update-munge symbol)]
-                                         (cond
-                                          ;; FIXME this is a bit of a hack to
-                                          ;; handle catch/finally. Should be
-                                          ;; generalized to other symbols but how
-                                          ;; to represent it?
-                                          (#{"catch" "finally"} symbol)
-                                          ,,(response/redirect
-                                             (format "/%s/%s/%s/%s/%s/"
-                                                     groupid artifactid version
-                                                     namespace "try"))
+                       (context "/:namespace" [namespace]
+                         (let-routes [t (thing/->T :namespace t namespace)]
+                           (GET "/" []
+                             (info log-msg)
+                             (v/namespace-page-memo type t))
 
-                                          ;; handle the case of redirecting due to munging
-                                          (not (= symbol symbol'))
-                                          ,,(response/redirect
-                                             (format "/store/%s/%s/%s/%s/%s/"
-                                                     groupid artifactid version
-                                                     namespace symbol'))
+                           (context "/:symbol" [symbol]
+                             (let-routes [t (thing/->T :def t symbol)]
+                               (GET "/" []
+                                 (let [symbol' (util/update-munge symbol)]
+                                   (cond
+                                     ;; FIXME this is a bit of a hack to
+                                     ;; handle catch/finally. Should be
+                                     ;; generalized to other symbols but how
+                                     ;; to represent it?
+                                     (#{"catch" "finally"} symbol)
+                                     ,,(response/redirect
+                                        (format "/%s/%s/%s/%s/%s/"
+                                                groupid artifactid version
+                                                namespace "try"))
 
-                                          :else
-                                          ,,(let [res (v/symbol-page type t)]
-                                              (info log-msg)
-                                              res))))
+                                     ;; handle the case of redirecting due to munging
+                                     (not (= symbol symbol'))
+                                     ,,(response/redirect
+                                        (format "/store/%s/%s/%s/%s/%s/"
+                                                groupid artifactid version
+                                                namespace symbol'))
 
-                                   (route/not-found
-                                    (fn [req]
-                                      (warn log-msg)
-                                      (v.e/error-unknown-symbol type t))))))
+                                     :else
+                                     ,,(let [res (v/symbol-page type t)]
+                                         (info log-msg)
+                                         res))))
 
-                             (route/not-found
-                              (fn [req]
-                                (warn log-msg)
-                                (v.e/error-unknown-namespace t)))))
+                               (route/not-found
+                                (fn [req]
+                                  (warn log-msg)
+                                  (v.e/error-unknown-symbol type t))))))
 
                          (route/not-found
                           (fn [req]
                             (warn log-msg)
-                            (v.e/error-unknown-version t)))))
+                            (v.e/error-unknown-namespace t)))))
 
                      (route/not-found
                       (fn [req]
                         (warn log-msg)
-                        (v.e/error-unknown-artifact t))))
+                        (v.e/error-unknown-version t)))))
 
-                   (route/not-found
-                    (fn [req]
-                      (warn log-msg)
-                      (v.e/error-unknown-group t)))))))
-         (routing req))))
+                 (route/not-found
+                  (fn [req]
+                    (warn log-msg)
+                    (v.e/error-unknown-artifact t))))
+
+               (route/not-found
+                (fn [req]
+                  (warn log-msg)
+                  (v.e/error-unknown-group t)))))))
+      (routing req))))
 
 ;; FIXME: Implement rather than stub
+(defmacro api-log []
+  `(info (-> ~'t
+           (select-keys [:uri])
+           (assoc :op ~'op))))
 
 (defroutes api-v0
   (context ["/api/v0"] []
     (GET "/" {{op :op} :params}
-      ;; op ∈ #{"groups"}
-      )
-
+      ((get v.api/root-ops op
+            v.api/unknown-op) nil))
+    
     (context ["/:group"] [group]
-      (GET "/" {{op :op} :params}
-        ;; op ∈ #{"artifacts" "notes" "meta"}
-        )
-
-      (context ["/:artifact"] [artifact]
+      (let-routes [t (thing/->Group group)]
         (GET "/" {{op :op} :params}
-          ;; op ∈ #{"versions" "notes" "meta"}
-          )
+          (api-log)
+          ((get v.api/group-ops op
+                v.api/unknown-op) t))
 
-        (context ["/:version"] [version]
-          (GET "/" {{op :op} :params}
-            ;; op ∈ #{"namespaces" "notes"}
-            )
-
-          (context ["/:namespace"] [namespace]
+        (context ["/:artifact"] [artifact]
+          (let-routes [t (thing/->Artifact t artifact)]
             (GET "/" {{op :op} :params}
-              ;; op ∈ #{"meta"
-              ;;        "notes"
-              ;;        "all"
-              ;;        "macros"
-              ;;        "vars"
-              ;;        "fns"
-              ;;        "specials"
-              ;;        "sentinels"}
-              )
+              (api-log)
+              ((get v.api/artifact-ops op
+                    v.api/unknown-op) t))
 
-            (context ["/:symbol"] [symbol]
-              (GET "/" {{op :op} :params}
-                ;; op ∈ #{"meta"
-                ;;        "doc"
-                ;;        "examples"}
-                ))))))))
+            (context ["/:version"] [version]
+              (let-routes [t (thing/->Version t version)]
+                (GET "/" {{op :op} :params}
+                  (api-log)
+                  ((get v.api/version-ops op
+                        v.api/unknown-op) t))
+
+                (context ["/:namespace"] [namespace]
+                  (let-routes [t (thing/->Ns t namespace)]
+                    (GET "/" {{op :op} :params}
+                      (api-log)
+                      ((get v.api/namespace-ops op
+                            v.api/unknown-op) t))
+                    
+                    (context ["/:symbol"] [symbol]
+                      (let-routes [t (thing/->Def t symbol)]
+                        (GET "/" {{op :op} :params}
+                          (api-log)
+                          ((get v.api/def-ops op
+                                v.api/unknown-op) t))))))))))))))
 
 (defroutes app
   (GET "/" {uri :uri}
@@ -179,7 +185,7 @@
 
   (GET "/about" []
     (v/markdown-page "articles/about"))
-  
+
   ;; The v0 API
   api-v0
 
---
+++ src/grimoire/web/views/api.clj
@@ -0,0 +1,163 @@
+(ns grimoire.web.views.api
+  (:require [grimoire.api :as api]
+            [grimoire.api.fs]
+            [grimoire.api.fs.read]
+            [grimoire.web.views :refer [site-config]]
+            [ring.util.response :refer [response]]))
+
+(defn fail [body]
+  (-> {:result :failure
+      :body   body}
+    response))
+
+(defn succeed [body]
+  (-> {:result :success
+      :body   body}
+    response))
+
+(defn unknown-op
+  "This function should yield a JSON error result indicating what the requested
+  unsupported operation was and what the path on which it was invoked was."
+  [thing op]
+  (-> {:op      op
+      :path    (:uri thing)
+      :message (str "Unknown op " op
+                    " on target " (:uri thing))}
+    fail))
+
+(declare list-groups
+         group-ops
+         artifact-ops
+         version-ops
+         ns-ops
+         def-ops)
+
+(def root-ops
+  {"groups" list-groups})
+
+(defn list-groups
+  "Returns a success result representing the "
+  [_]
+  (-> (for [g (api/list-groups site-config)]
+       {:name (:name g)
+        :html (str "/store/" (:uri g))
+        :children (->> (for [op (keys group-ops)]
+                       [op (str "/api/v0/" (:uri g) "?op=" op)])
+                    (into {}))})
+    succeed))
+
+(declare group-notes
+         group-meta
+         group-artifacts
+         artifact-ops)
+
+(def group-ops
+  {"artifacts" group-artifacts
+   "notes"     group-notes
+   "meta"      group-meta})
+
+(defn group-notes
+  "Returns the notes, if any, for the target group."
+  [thing]
+  (->> thing
+    (api/read-notes site-config)
+    succeed))
+
+(defn group-meta
+  "Returns the metadata, if any, for the target group."
+  [thing]
+  (->> thing
+    (api/read-meta site-config)
+    succeed))
+
+(defn group-artifacts
+  "Returns the artifacts for the target group."
+  [group-thing]
+  (-> (for [t (api/list-artifacts site-config group-thing)]
+       {:name (:name t)
+        :html (str "/store/" (:uri t))
+        :children (->> (for [op (keys artifact-ops)]
+                       [op (str "/api/v0/" (:uri t) "?op=" op)])
+                    (into {}))})
+    succeed))
+
+(declare artifact-versions
+         namespace-ops)
+
+(def artifact-ops
+  {"versions" artifact-versions
+   "notes"    group-notes
+   "meta"     group-meta})
+
+(defn artifact-versions
+  "Returns the versions for the target artifact."
+  [artifact-thing]
+  (-> (for [t (api/list-versions site-config artifact-thing)]
+       {:name (:name t)
+        :html (str "/store/" (:uri t))
+        :children (->> (for [op (keys version-ops)]
+                       [op (str "/api/v0/" (:uri t) "?op=" op)])
+                    (into {}))})
+    succeed))
+
+(declare version-namespaces)
+
+(def version-ops
+  {"namespaces" version-namespaces
+   "notes"      group-notes
+   "meta"       group-meta})
+
+(defn version-namespaces
+  "Returns a Succeed of the namespaces in the target version."
+  [version-thing]
+  (-> (for [t (api/list-namespaces site-config version-thing)]
+       {:name (:name t)
+        :html (str "/store/" (:uri t))
+        :children (->> (for [op (keys namespace-ops)]
+                       [op (str "/api/v0/" (:uri t) "?op=" op)])
+                    (into {}))})
+    succeed))
+
+(declare def-ops
+         namespace-search)
+
+(def namespace-ops
+  {"all"       (partial namespace-search identity)
+   "macros"    (partial namespace-search #{:macro})
+   "vars"      (partial namespace-search #{:var})
+   "fns"       (partial namespace-search #{:fn})
+   "specials"  (partial namespace-search #{:special})
+   "sentinels" (partial namespace-search #{:sentinel})
+   "notes"     group-notes
+   "meta"      group-meta})
+
+;; FIXME: memoize/cache this shit b/c slow as fuck
+(defn namespace-search
+  "Returns a Succeed listing the defs of the target namespace and the supported
+  operations thereon."
+  [filter ns-thing]
+  (-> (for [t     (api/list-defs site-config ns-thing)
+           :let  [meta (api/read-meta site-config t)]
+           :when (filter (get t :type :fn))]
+       {:name (:name t)
+        :html (str "/store/" (:uri t))
+        :children (->> (for [op (keys def-ops)]
+                       [op (str "/api/v0/" (:uri t) "?op=" op)])
+                    (into {}))})
+    succeed))
+
+(declare def-examples)
+
+(def def-ops
+  {"notes"    group-notes
+   "meta"     group-meta
+   "examples" def-examples})
+
+(defn def-examples
+  "Returns a Succeed of examples for the given def, if any exist in the
+  datastore."
+  [def-thing]
+  (->> def-thing
+    (api/read-examples site-config)
+    (map second)
+    succeed))

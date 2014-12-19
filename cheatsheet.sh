62f271c3cb146a1327e028716dc5b9dcfca73d8d
arrdem <me@arrdem.com>
2014-12-19 17:30:01 -0600

    Use dat/ for the scratch dir

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 set -e
+mkdir -p dat
+pushd dat
 
 if [ ! -d "clojure-cheatsheets" ]; then
   git clone https://github.com/arrdem/clojure-cheatsheets
 fi
 
-pushd .
-cd clojure-cheatsheets/
+pushd clojure-cheatsheets/
 git reset --hard && git clean -fd && git pull origin master
 
 cd src/clj-jvm
 lein run links-to-grimoire
 popd
-
-cp clojure-cheatsheets/src/clj-jvm/cheatsheet-full.html .
+popd
+cp dat/clojure-cheatsheets/src/clj-jvm/cheatsheet-full.html .
 sed  -i -e 1,3d cheatsheet-full.html
 head -n -2 cheatsheet-full.html > _cheatsheet
 mv _cheatsheet cheatsheet-full.html

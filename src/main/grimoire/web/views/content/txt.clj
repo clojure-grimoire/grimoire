863e99ba5fac4810c95138ba0bbeef79a64f8df4
arrdem <me@arrdem.com>
2014-12-22 13:56:01 -0600

    Implement artifact-page for text/plain
    Work on #151

--- src/main/grimoire/web/views/content/txt.clj
+++ src/main/grimoire/web/views/content/txt.clj
@@ -19,6 +19,13 @@
      response/response
      (response/content-type "text/plain")))
 
+(defmethod artifact-page :text/plain [_ artifact-thing]
+  (-> (str "# Artifact " (:uri artifact-thing) "\n\n"
+          "Sorry, this page isn't supported in plaintext mode :c\n"
+          "Feel free to file an issue on the bugtracker if you think this could be useful\n")
+     response/response
+     (response/content-type "text/plain")))
+
 (defmethod symbol-page :text/plain [_ def-thing]
   (let [groupid    (t/thing->group     def-thing)
         artifactid (t/thing->artifact  def-thing)

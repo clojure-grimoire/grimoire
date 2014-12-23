1861c67087ae49c54951748807e4afb5f1b67f63
arrdem <me@arrdem.com>
2014-12-23 17:47:26 -0600

    Upgrade deps, exclude clojure

--- project.clj
+++ project.clj
@@ -6,17 +6,28 @@
   :aliases {"serve" ["with-profile" "server" "run"]}
   :profiles {:repl   {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}
              :server {:dependencies [[org.clojure/clojure "1.6.0"]
-                                     [org.clojure-grimoire/lib-grimoire "0.6.4"]
-                                     [cheshire "5.3.1"]
-                                     [com.taoensso/timbre "3.2.1"]
-                                     [ring/ring-core "1.3.0"]
-                                     [ring/ring-jetty-adapter "1.3.0"]
-                                     [selmer "0.6.9"]
-                                     [compojure "1.1.8"]
-                                     [hiccup "1.0.5"]
-                                     [markdown-clj "0.9.47"]
-                                     [me.raynes/conch "0.8.0"]
-                                     [me.arrdem/detritus "0.2.0"]]
+                                     [org.clojure-grimoire/lib-grimoire "0.6.4"
+                                      :exclusions [org.clojure/clojure]]
+                                     [cheshire "5.4.0"
+                                      :exclusions [org.clojure/clojure]]
+                                     [com.taoensso/timbre "3.3.1"
+                                      :exclusions [org.clojure/clojure]]
+                                     [ring/ring-core "1.3.2"
+                                      :exclusions [org.clojure/clojure]]
+                                     [ring/ring-jetty-adapter "1.3.2"
+                                      :exclusions [org.clojure/clojure]]
+                                     [selmer "0.7.7"
+                                      :exclusions [org.clojure/clojure]]
+                                     [compojure "1.3.1"
+                                      :exclusions [org.clojure/clojure]]
+                                     [hiccup "1.0.5"
+                                      :exclusions [org.clojure/clojure]]
+                                     [markdown-clj "0.9.58"
+                                      :exclusions [org.clojure/clojure]]
+                                     [me.raynes/conch "0.8.0"
+                                      :exclusions [org.clojure/clojure]]
+                                     [me.arrdem/detritus "0.2.0"
+                                      :exclusions [org.clojure/clojure]]]
                       :main         grimoire.web.service}
              :dev    {:source-paths ["src/dev"]
                       :main         user}

6d9dea7991fb9f17980ef3034ec35140d43ed671
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-02-23 17:04:09 -0600

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,36 +6,36 @@
 
   :source-paths ["src/main"]
   
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"
                   :exclusions [org.clojure/clojure]]
-                 [org.clojure-grimoire/lib-grimoire "0.10.3"
+                 [org.clojure-grimoire/lib-grimoire "0.10.4"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure-grimoire/simpledb "0.1.10"
                   :exclusions [org.clojure/clojure]]
                  [cheshire "5.5.0"
                   :exclusions [org.clojure/clojure]]
-                 [com.taoensso/timbre "4.1.4"
+                 [com.taoensso/timbre "4.2.1"
                   :exclusions [org.clojure/clojure]]
                  [ring/ring-core "1.4.0"
                   :exclusions [org.clojure/clojure]]
                  [ring/ring-jetty-adapter "1.4.0"
                   :exclusions [org.clojure/clojure]]
-                 [selmer "0.9.5"
+                 [selmer "1.0.0"
                   :exclusions [org.clojure/clojure]]
                  [compojure "1.4.0"
                   :exclusions [org.clojure/clojure]]
                  [hiccup "1.0.5"
                   :exclusions [org.clojure/clojure]]
-                 [markdown-clj "0.9.80"
+                 [markdown-clj "0.9.85"
                   :exclusions [org.clojure/clojure]]
                  [me.raynes/conch "0.8.0"
                   :exclusions [org.clojure/clojure]]
-                 [sitemap "0.2.4"
+                 [sitemap "0.2.5"
                   :exclusions [org.clojure/clojure]]
                  [instaparse "1.4.1"
                   :exclusions [org.clojure/clojure]]
-                 [environ "1.0.1"
+                 [environ "1.0.2"
                   :exclusions [org.clojure/clojure]]]
   
   :aliases {"serve" ["with-profile" "server" "run"]}

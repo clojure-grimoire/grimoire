70a9486fb82d921b2495a5f750d3322566a5b00c
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-14 00:17:00 -0800

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -6,48 +6,41 @@
 
   :source-paths ["src/main"]
   :plugins      [[lein-environ "1.1.0"]]
-  :exclusions   [org.clojure/clojure]
-  :dependencies [[org.clojure/core.match "0.3.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure-grimoire/lib-grimoire "0.10.9"]
-                 [org.clojure-grimoire/simpledb "0.1.10"]
-                 [cheshire "5.5.0"]
-                 [com.taoensso/timbre "4.2.1"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [selmer "1.0.0"]
-                 [compojure "1.4.0"]
+                 [org.clojure-grimoire/simpledb "0.2.1"]
+                 [cheshire "5.8.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [selmer "1.11.3"]
+                 [compojure "1.6.0"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "0.9.85"]
+                 [markdown-clj "1.0.1"]
                  [me.raynes/conch "0.8.0"]
                  [sitemap "0.2.5"]
-                 [instaparse "1.4.1"]
-                 [environ "1.0.2"]
-                 [pandect "0.5.4"]
-                 [clj-fuzzy "0.3.0"]]
+                 [instaparse "1.4.8"]
+                 [environ "1.1.0"]
+                 [pandect "0.6.1"]
+                 [clj-fuzzy "0.4.1"]]
 
   :aliases {"serve" ["with-profile" "server" "run"]}
 
-  :profiles {:clj-1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :jnt-0.1 {:dependencies [[org.jaunt-lang/jaunt "0.1.0"]]}
-             :jnt-0.2 {:dependencies [[org.jaunt-lang/jaunt "0.2.0"]]}
-             :jnt-0.3 {:dependencies [[org.jaunt-lang/jaunt "0.3.0-SNAPSHOT"]]}
-
-             :env-defaults {:env {:fundraising-active      False
+  :profiles {:env-defaults {:env {:fundraising-active      False
                                   :fundraising-rate        10
                                   :simpledb-file           "analytics.db"
                                   :simpledb-write-interval 1
                                   :grimoire-verson         :project/version}}
 
-             :server [:clj-1.8
-                      :env-defaults
+             :server [:env-defaults
                       {:env {:url           "https://www.conj.io"
                              :simpledb-file "/srv/www/grimoire/analytics.db"}}
                       {:main grimoire.web.service}]
 
-             :dev [:clj-1.8
-                   :env-defaults
+             :dev [:env-defaults
                    {:env {:url "http://127.0.0.1:3000"
                           :dev true}}
-                   {:dependencies [[ring/ring-mock "0.3.0"]]
+                   {:dependencies [[ring/ring-mock "0.3.2"]]
                     :source-paths ["src/dev"]
                     :main         user}]})
